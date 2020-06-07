d31b9fec7e9b19440a63dafd4c66c470b979061e
TomasR264 <tomasrossi264@gmail.com>
2020-06-07 18:07:36 -0300

    bandas y generos modelados

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,3 +2,55 @@ module Lib where
 import Text.Show.Functions
 
 laVerdad = True
+
+
+data Festival = Festival {lugarDondeSeRealiza :: String,
+                          cantidadDeGente :: Float,
+                          animoInicialDelPublico :: String,
+                          bandas :: [Banda]
+                          } deriving (Show)
+
+
+
+
+data Banda = Banda {nombre :: String,
+                    genero :: String
+                   } deriving (Show)
+
+
+
+
+tocar :: Banda -> Festival -> Festival
+tocar banda festival = festival {cantidadDeGente = modifGenteSegunGenero banda festival, 
+                                 animoInicialDelPublico = modifAnimoSegunGenero banda festival}
+
+
+
+modifGenteSegunGenero :: Banda -> Festival -> Float
+modifGenteSegunGenero banda festival 
+ | genero banda == "rock nacional" = cantidadDeGente festival + 100
+ | genero banda == "pop" = popSegunAnimo festival 
+ | otherwise = cantidadDeGente festival + (cantidadDeGente festival * 0.01)
+
+popSegunAnimo :: Festival -> Float
+popSegunAnimo festival
+ | animoInicialDelPublico festival == "indiferente" = (cantidadDeGente festival) *2
+ | otherwise = cantidadDeGente festival
+ 
+modifAnimoSegunGenero :: Banda -> Festival -> String
+modifAnimoSegunGenero banda festival 
+ | genero banda == "pop" = "euforico"
+ | otherwise = metalSegunSubGenero banda festival
+
+
+metalSegunSubGenero :: Banda -> Festival -> String
+metalSegunSubGenero banda festival 
+ | genero banda == "Heavy metal" = animoInicialDelPublico festival ++ "pesado"
+ | genero banda == "Trash metal" = animoInicialDelPublico festival ++ "basura"
+ | otherwise = animoInicialDelPublico festival ++ (take 5 (genero banda))
+
+
+
+
+
+
